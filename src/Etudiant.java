c8a24b433c1994afad336d0836471e90a29ee5e2
Mohammed Yassin <yssnmed@gmail.com>
2024-10-25 14:58:23 +0100

    TP Etudiant

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,4 +1,31 @@
 
 public class Etudiant {
+	private String CNE;
+	private String Nom ;
+	private double Moyenne ;
+	private int classement  ;
+	private String decision ;
+	
+	public Etudiant ( String cne , String nom ,double moyenne ) {
+		CNE = cne ;
+		Nom = nom ;
+		Moyenne = moyenne ;	
+	}
+	public double getMoyenne() {
+		return Moyenne ;
+	}
+	public String getNom() {
+		return  Nom ;
+	}
+	public String getCne() {
+		return CNE ;
+	}
+	public int getClassement() {
+        return classement;
+    }
 
+    public void setClassement(int classement) {
+        this.classement = classement;
+    }
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List <Etudiant> filiere_A  = new ArrayList<Etudiant>();
 		
+		Etudiant etudiant1 = new Etudiant("L232323" , "Mohammed yassine ", 17);
+		Etudiant etudiant2 = new Etudiant("L242424" , "Ismail ", 17);
+		Etudiant etudiant3 = new Etudiant("L232323" , "ouass ", 14);
+		Etudiant etudiant4 = new Etudiant("L242424" , "zaid ", 17);
+		filiere_A.add(etudiant1);
+		filiere_A.add(etudiant2);
+		filiere_A.add(etudiant3);
+		filiere_A.add(etudiant4);
+		List<Etudiant> Admis_A = new ArrayList<Etudiant>();
+		List<Etudiant> redoublants_A = new ArrayList<Etudiant>();
+		List<Etudiant> ratrrapage_A = new ArrayList<Etudiant>();
+
+//		filiere
+		for(Etudiant i: filiere_A) {
+			if(i.getMoyenne()>=10) {
+				Admis_A.add(i);
+			}
+			else if(i.getMoyenne()>=7) {
+				ratrrapage_A.add(i);
+			}
+			else if(i.getMoyenne()<7) {
+				redoublants_A.add(i);
+			}
+		}
+}
+// mooyenne
+		  public static double calculerMoyenneGenerale(List<Etudiant> etudiants) {
+		        double somme = 0;
+		        for (Etudiant etudiant : etudiants) {
+		            somme += etudiant.getMoyenne();
+		        }
+		        return somme / etudiants.size(); }
 	}
 
-}
+	
